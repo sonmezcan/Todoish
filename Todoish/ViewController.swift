0b965df4952921ec756568878b45eb40196ea0c2
Mevlüt Sönmez <sonmezcan1997@gmail.com>
2024-09-14 09:42:55 +0300

    Initial Commit

---
+++ Todoish/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Todoish
+//
+//  Created by can on 14.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
